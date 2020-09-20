1aeeb0b2357b56890836dd3e6bd24f9edd5cc362
Dominick John <alpineboarder@gmail.com>
2020-09-20 12:08:43 -0400

    Added test movement controls and camera

---
+++ Assets/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/*
+Attaches to Main Camera object
+Allows it to follow Transforms with offsets
+*/
+
+public class GameCamera : MonoBehaviour
+{
+    public Transform cameraTarget;    // Target that the camera will be following
+    public Vector3 positionOffset;    // Offset of camera from the target's position
+    public float smoothAmount = 0.1f;    // How smoothly camera will go to target position, 0 = instant
+
+    private Vector3 _velocity; // Used to store velocity of camera movement for smoothing
+
+    private void LateUpdate()
+    {
+        // Smoothly move to target + offset
+        transform.position = Vector3.SmoothDamp(transform.position, cameraTarget.transform.position + positionOffset, ref _velocity, smoothAmount);
+    }
+}
---
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/*
+Controls the player input and moves character on screen
+*/
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;    // Where to send animation data to
+    public float _moveSpeed = 1f;    // How fast player will move
+    
+    private CharacterController _characterController;
+    private Vector3 _facingDirection = Vector3.back;
+    private Vector3 _moveVector;
+    
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Get player input data
+        _moveVector.x = Input.GetAxis("Horizontal");
+        _moveVector.z = Input.GetAxis("Vertical");
+        
+        // Prevent movement from exceeding a factor of 1, useful for diagonals.
+        _moveVector = Vector3.ClampMagnitude(_moveVector, 1f);
+
+        // If moving, change the facing direction
+        if (_moveVector != Vector3.zero)
+        {
+            _facingDirection = _moveVector.normalized;
+        }
+
+        // Update the character controller to perform the move
+        _characterController.SimpleMove(_moveVector * _moveSpeed);
+        
+        // Send animator the movement data for visuals
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        animator.SetFloat("x", _facingDirection.x);
+        animator.SetFloat("y", _facingDirection.z);
+        animator.SetFloat("speed", _moveVector.magnitude);
+    }
+}
