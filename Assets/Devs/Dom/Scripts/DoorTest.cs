6a596e1c5c847fb16668451a8540e7a91cf3e512
Dominick John <alpineboarder@gmail.com>
2020-09-27 10:01:07 -0400

    Added DoorTest

---
+++ Assets/Devs/Dom/Scripts/DoorTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTest : MonoBehaviour, Interactable
+{
+    public bool open = false;
+    public float doorSpeed = 2f;
+
+    private Vector3 _closedPosition;
+
+    private void Start()
+    {
+        _closedPosition = transform.position;
+    }
+
+    public void OnUse()
+    {
+        open = !open;
+    }
+
+    private void Update()
+    {
+        Vector3 targetPos;
+        
+        if (open)
+        {
+            targetPos = new Vector3(transform.position.x, _closedPosition.y - 1.45f, transform.position.z);
+        }
+        else
+        {
+            targetPos = _closedPosition;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * doorSpeed);
+    }
+
+}
