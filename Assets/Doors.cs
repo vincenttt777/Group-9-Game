5f036bb02264022784f7b1c8b3fe1b606ae2f2f8
Cade Cabrera <cabreracade@gmail.com>
2020-09-27 21:49:11 -0400

    MotionDoor

---
+++ Assets/Doors.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+
+    void Start(){
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+
+    }    
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Player"){
+            doorOpen = true;
+            DoorControl("Open");
+        }
+    }
+
+    void OnTriggerExit(Collider col){
+        if(col.gameObject.tag == "Player"){
+            doorOpen = false;
+            DoorControl("Close");
+        }
+    }
+
+    void DoorControl(string direction){
+        animator.SetTrigger(direction);
+
+    }
+}
