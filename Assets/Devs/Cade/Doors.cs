7c295beb16c589ed84f3b4d7433904ece6951604
Cade Cabrera <cabreracade@gmail.com>
2020-10-05 20:48:19 -0400

    Chests/Interactions

--- Assets/Devs/Cade/Doors.cs
+++ Assets/Devs/Cade/Doors.cs
@@ -5,24 +5,19 @@ using UnityEngine;
 public class Doors : MonoBehaviour
 {
     Animator animator;
-    //bool doorOpen;
 
     void Start(){
-        //doorOpen = false;
         animator = GetComponent<Animator>();
-
     }    
 
     void OnTriggerEnter(Collider col){
         if(col.gameObject.tag == "Player"){
-            //doorOpen = true;
             DoorControl("Open");
         }
     }
 
     void OnTriggerExit(Collider col){
         if(col.gameObject.tag == "Player"){
-            //doorOpen = false;
             DoorControl("Close");
         }
     }
