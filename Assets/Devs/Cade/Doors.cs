b4dfe6ddbfa8997864cac11539fbefffb6b580b8
Cade Cabrera <cabreracade@gmail.com>
2020-10-12 20:49:41 -0400

    Inventory and Item Pickup
    made a player inventory and the ability for items to be picked up into inventory

--- Assets/Devs/Cade/Doors.cs
+++ Assets/Devs/Cade/Doors.cs
@@ -7,23 +7,24 @@ public class Doors : MonoBehaviour
     Animator animator;
 
     void Start(){
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); //grab the animation
     }    
 
     void OnTriggerEnter(Collider col){
-        if(col.gameObject.tag == "Player"){
-            DoorControl("Open");
+        if(col.gameObject.tag == "Player"){ //if player moves into collider zone
+            DoorControl("Open"); // pass open to door control 
         }
     }
 
     void OnTriggerExit(Collider col){
-        if(col.gameObject.tag == "Player"){
-            DoorControl("Close");
+        if(col.gameObject.tag == "Player"){ //same collider as enter
+            DoorControl("Close"); // pass close to the door control
         }
     }
 
     void DoorControl(string direction){
-        animator.SetTrigger(direction);
+        animator.SetTrigger(direction); // if open is passed to this it will open the door
+                                        // if close is passed it will trigger the close animation
 
     }
 }
---
+++ Assets/Devs/Cade/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+    
+    private void Start(){
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter(Collider tag){
+        if (tag.CompareTag("Player")){
+            for (int i = 0; i < inventory.slots.Length; i++){
+                if (inventory.isFull[i]==false){
+                    inventory.isFull[i]=true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
