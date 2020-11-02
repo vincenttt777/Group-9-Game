6970cca677546e26a079ee8d71f8099ce391bfd5
Cade Cabrera <cabreracade@gmail.com>
2020-11-01 20:34:29 -0500

    working on dropping/using items in inventory

---
+++ Assets/Devs/Cade/Slot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+   public void DropItem(){
+       foreach (Transform child in transform ){
+           GameObject.Destroy(child.gameObject);
+       }
+   }
+/*
+   public void potionClick(){
+       Game.GetPlayerTransform().GetComponent<Player>().Health += 1; 
+   }
+   */
+}
