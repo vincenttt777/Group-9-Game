7e378e7725f30b8c066f12629c38f4a5e46559e0
Cade Cabrera <cabreracade@gmail.com>
2020-11-16 13:44:52 -0500

    Added More Potions
    Speed and Invulnerability Potions

--- Assets/Devs/Cade/Slot.cs
+++ Assets/Devs/Cade/Slot.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Slot : MonoBehaviour
 {
+    private Inventory inventory;
    public void DropItem(){
         GameObject.Destroy(gameObject);
+        
    }
 
    public void potionClick(){
-       Debug.Log("test");
        Game.GetPlayerTransform().GetComponent<Player>().Health += 1; 
+   }
+
+   public void speedClick(){
+       Game.Player.SetPlayerSpeedFactor(2,10);
+   }
+
+   public void invulClick(){
+       Game.Player.SetPlayerInvulnerability(10);
    }
    
 }
