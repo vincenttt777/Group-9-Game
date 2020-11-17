698032a229bce10deeb91866ec277d414b0cc4fb
Cade Cabrera <cabreracade@gmail.com>
2020-11-16 19:46:11 -0500

    Chests Spawn Objects (working fully now)

--- Assets/Devs/Cade/Pickup.cs
+++ Assets/Devs/Cade/Pickup.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
-
+using TMPro;
 public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
@@ -9,6 +11,8 @@ public class Pickup : MonoBehaviour
     
     private void Start(){
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        transform.DOMoveY(transform.position.y + 0.2f, 1f).SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
     }
 
     void OnTriggerEnter(Collider tag){
