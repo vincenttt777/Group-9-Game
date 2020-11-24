315eb8a9b6bcc8dd3219cc808f07cdce5ecc27c5
vincenttt777 <72100032+vincenttt777@users.noreply.github.com>
2020-11-24 17:29:39 -0500

    asd

---
+++ Assets/Devs/Diego/Scripts/DoorLightTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLightTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    bool isOpened = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(!isOpened)
+        {
+            isOpened = true;
+            door.transform.position += new Vector3(0, 2, 0);
+        }
+    }
+}
