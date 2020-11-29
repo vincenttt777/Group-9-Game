bb140deaf801d21d26f28f0fdd5e155313ceebf6
vincenttt777 <72100032+vincenttt777@users.noreply.github.com>
2020-11-29 15:58:19 -0500

    Working on final level still! 70% complete

---
+++ Assets/Devs/Diego/Scripts/DoorScriptFinal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScriptFinal : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    bool isOpened = false;
+
+    void OnTriggerStay(Collider col)
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpened)
+            {
+                isOpened = true;
+                door.transform.position -= new Vector3(0, 2, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Devs/Diego/Scripts/InstructionText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionText : MonoBehaviour
+{
+    public GameObject OpenPanel = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(false);
+        }
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    void Update()
+    {
+        if(IsOpenPanelActive)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                OpenPanel.SetActive(false);
+            }
+        }
+    }
+}
